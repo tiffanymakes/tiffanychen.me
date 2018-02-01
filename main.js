d82e16d915afb1d3f8c3f084d7f42aca9a6490d0
Tiffany <tiffanyc.lehigh@gmail.com>
2018-02-01 15:52:56 -0500

    Added images and robots.txt. Changed Schema markup to JSON-LD format. Changed above-the-fold-content.
    Added robots.txt file to prevent web crawlers from crawling files in "images" folder.
    
    Changed to JSON-LD markup so all information is in the header rather than inline with text.
    
    Changed the above-the-fold content to only be the "about" section and included the critical CSS in the HTML file.

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 
+// Footer year
 const year = new Date().getFullYear();
 document.getElementById('year').innerHTML = year;
 
+// Menu toggle in mobile
+document.querySelector('#nav-icon').addEventListener('click', menuToggle);
 function menuToggle() {
 	document.getElementById('nav').classList.toggle('open-nav');
     document.getElementById('nav').classList.toggle('close-nav');
 }
-document.querySelector('#nav-icon').addEventListener('click', menuToggle);
 
-
-
-/* References:
-** https://codepen.io/geertjanhendriks/pen/yitop
-https://codepen.io/bradfrost/pen/sHvaz
-https://codepen.io/davidcochran/pen/MYxyvz
-https://codepen.io/hanlinC/pen/dJwil
-*/

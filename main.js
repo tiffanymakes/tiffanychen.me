4b8b04a5d2cb790c5916781eb92206d2f647ea3f
Tiffany <tiffanyc.lehigh@gmail.com>
2018-02-09 13:58:39 -0500

    Added scroll button and updated website description
    Added a scroll button that appears after the page is scrolled 20px from the top. When it is clicked, it jumps to the top of the page. The CSS scroll behavior property was set as smooth.
    
    Changed JS to use querySelector in all cases instead of getElementById.
    
    Re-wrote website description and added a meta description.
    
    Removed "close-nav" class from HTML. The navigation button still seems to work.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-
 // Footer year
 const year = new Date().getFullYear();
-document.getElementById('year').innerHTML = year;
+document.querySelector('#year').innerHTML = year;
 
 // Menu toggle in mobile
-document.querySelector('#nav-icon').addEventListener('click', menuToggle);
-function menuToggle() {
-	document.getElementById('nav').classList.toggle('open-nav');
-    document.getElementById('nav').classList.toggle('close-nav');
+document.querySelector('#nav-button').addEventListener('click', navButton);
+function navButton() {
+	document.querySelector('#nav').classList.toggle('open-nav');
+	document.querySelector('#nav').classList.toggle('close-nav');
 }
 
+// Show scroll button when page is scrolled 20px from top
+window.addEventListener('scroll', scrollButton);
+function scrollButton() {
+	document.querySelector('#scroll-button').style.display =
+	document.documentElement.scrollTop > 20 ? 'block' : 'none';
+}
+
+// Scroll to top when user clicks on scroll button
+document.querySelector('#scroll-button').addEventListener('click', toTop);
+function toTop() {
+  window.scroll(0,0);
+}
